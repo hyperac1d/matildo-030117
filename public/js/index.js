f88f973a51d0f273e9c2396170ff76e80459f1db
hyperac1d <dlastgamer18@gmail.com>
2017-03-03 22:42:01 +0800

    my first commit

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+function clickMe(){
+
+
+document.getElementById('first').innerHTML = "Tyrone";
+
+}
+
+
+
+function addMe(){
+
+
+
+  var x = document.getElementById("fn").value;
+  var y = document.getElementById("sn").value;
+  var sum = parseInt(x)+ parseInt(y);
+  document.getElementById("answer").innerHTML = sum;
+
+}
